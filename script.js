430c58c28a6b6c3e06a4e850ef1ffef17cfdfa85
JuanRpo07 <estebanrestrepo04@gmail.com>
2025-11-13 19:44:02 -0500

    creo repositorio

---
+++ script.js
@@ -0,0 +1,22 @@
+"Use Strict";
+
+//Defino los controles
+const txtGrados = document.getElementById("txtGrados");
+const btnConvertir = document.getElementById("btnConvertir");
+const spanResultados = document.getElementById("spanResultados");
+
+btnConvertir.addEventListener("click", () => {
+  let mensaje;
+  let celsius = txtGrados.value;
+  if (celsius === "") {
+    mensaje = "No ingresaste un valor";
+  } else {
+    celsius = Number(txtGrados.value);
+    let fahrenheit = celsius * 1.8 + 32;
+    mensaje = `${fahrenheit.toFixed(2)} °F`;
+  }
+
+  txtGrados.textContent = "";
+  spanResultados.textContent = mensaje;
+  
+});
